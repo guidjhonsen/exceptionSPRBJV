cdf9b857669b3da0db09a3e472b54c9e6a296eac
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-08-02 23:46:45 -0400

    78. Creando Proyecto

---
+++ springboot-error/src/test/java/com/guidjhonsen/curso/springboot/error/springbooterror/springboot_error/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidjhonsen.curso.springboot.error.springbooterror.springboot_error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
