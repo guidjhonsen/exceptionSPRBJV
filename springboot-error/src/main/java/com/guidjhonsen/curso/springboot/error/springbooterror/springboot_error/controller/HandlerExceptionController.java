d8a270ad356daed3bc3ce59f165256eaefade4e8
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-08-03 22:46:05 -0400

    82. Personalizando error 404

---
+++ springboot-error/src/main/java/com/guidjhonsen/curso/springboot/error/springbooterror/springboot_error/controller/HandlerExceptionController.java
@@ -0,0 +1,39 @@
+package com.guidjhonsen.curso.springboot.error.springbooterror.springboot_error.controller;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import com.guidjhonsen.curso.springboot.error.springbooterror.springboot_error.models.Error;
+
+@RestControllerAdvice
+public class HandlerExceptionController {
+
+    @ExceptionHandler({ArithmeticException.class})
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Division by zero");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        //return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFound(NoHandlerFoundException ex) {
+        
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrado");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        //return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+}
